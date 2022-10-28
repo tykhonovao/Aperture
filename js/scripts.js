5577e5a33a7a965f22c62c129322e6e653d66683
tykhonovao <tykhonova.o.o@gmail.com>
2022-10-29 01:25:31 +0300

    new sections and owl carousel

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: false,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
